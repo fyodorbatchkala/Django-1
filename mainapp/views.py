ca5f4756675b2345509768c321e3440409b446f4
fbatch <fyodor.batchkala@gmail.com>
2021-07-14 14:22:52 +0300

    lesson_1

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainapp/products.html')
+
+def contacts(request):
+    return render(request, 'mainapp/contact.html')
