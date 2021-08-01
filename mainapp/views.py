12e9ddcf5643e1f1a91a8a20a427f16f213cd5d6
fbatch <fyodor.batchkala@gmail.com>
2021-08-01 16:56:11 +0300

    add lesson_5

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,26 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from mainapp.models import Product
+from basketapp.models import Basket
+from mainapp.models import Product, ProductCategory
 
 
-def index(request):
+def products(request, pk=None):
+    title = 'продукты'
 
-    title = 'каталог'
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()[:4]
 
-    links_menu = [
-        {'href': 'index', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    basket = []
 
-    products = Product.objects.all()[:4]
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'related_products': same_products,
+            'products': products,
+            'basket': basket,
+        }
+
+        return render(request, 'mainapp/products.html', context)
+
+    products = Product.objects.all().order_by('price')
 
     context = {
         'title': title,
         'links_menu': links_menu,
-        'related_products': products,
+        'related_products': same_products,
+        'products': products,
+        'basket': basket,
     }
     return render(request, 'mainapp/products.html', context)
